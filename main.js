d1a00d4cb25458ab87df15e507ddb8dda8425372
Smoke52ru <60500815+Smoke52ru@users.noreply.github.com>
2021-10-15 18:31:01 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 player1 = {
-    player: 1,
     name: "Liu Kang",
-    hp: 100,
+    hp: 50,
     img: 'http://reactmarathon-api.herokuapp.com/assets/liukang.gif',
     weapon: ['fists', 'sword'],
     attack: function () {
@@ -9,9 +8,8 @@ player1 = {
     }
 }
 player2 = {
-    player: 2,
     name: "Sonya",
-    hp: 100,
+    hp: 60,
     img: 'http://reactmarathon-api.herokuapp.com/assets/sonya.gif',
     weapon: ['fists', 'pistol'],
     attack: function () {
@@ -19,24 +17,25 @@ player2 = {
     }
 }
 
-function createElement(tag, className) {
-    const $tag = document.createElement(tag)
-    if (className) {
-        $tag.classList.add(className)
-    }
-    return $tag
-}
+function createPlayer(playerClass, playerObj){
+    let $root = document.createElement('div')
+    $root.classList.add(playerClass)
 
-function createPlayer(playerObj){
-    let $root = createElement('div', 'player'+playerObj.player)
-    let $bar = createElement('div', 'progressbar')
-    let $life = createElement('div', 'life')
-    let $name = createElement('div', 'name')
-    let $character = createElement('div', 'character')
-    let $img = createElement('img')
+    let $bar = document.createElement('div')
+    $bar.classList.add('progressbar')
 
+    let $life = document.createElement('div')
+    $life.classList.add('life')
     $life.style.width = playerObj['hp'] + '%'
+
+    let $name = document.createElement('div')
+    $name.classList.add('name')
     $name.innerText = playerObj['name']
+
+    let $character = document.createElement('div')
+    $character.classList.add('character')
+
+    let $img = document.createElement('img')
     $img.src = playerObj['img']
 
     $bar.appendChild($life)
@@ -48,48 +47,9 @@ function createPlayer(playerObj){
     return $root
 }
 
-const $arenas = document.querySelector('div.arenas')
-    let $p1 = createPlayer(player1)
-    let $p2 = createPlayer(player2)
+let $p1 = createPlayer('player1', player1)
+let $p2 = createPlayer('player2', player2)
+
+let $arenas = document.querySelector('div.arenas')
     $arenas.appendChild($p1)
     $arenas.appendChild($p2)
-
-let randomDamage = () => Math.ceil(Math.random()*20)
-
-
-function changeHP(player) {
-    const $playerHP = document.querySelector('.player'+ player.player +' .life')
-    player.hp -= randomDamage()
-    $playerHP.style.width = player.hp + '%'
-
-    if (player.hp <= 0) {
-        player.hp = 0
-        $playerHP.style.width = player.hp + '%'
-        // $arenas.appendChild(playerLose(player.name))
-        $arenas.appendChild(playerWin(player.player === 1 ? player2.name : player1.name))
-
-        $randomButton.disabled = true
-    }
-}
-
-function playerLose(name) {
-    const $loseTitle = createElement('div', 'loseTitle')
-    $loseTitle.innerText = name +' lose'
-
-    return $loseTitle
-}
-
-function playerWin(name) {
-    const $winTitle = createElement('div', 'loseTitle')
-    $winTitle.innerText = name + ' wins'
-
-    return $winTitle
-}
-
-const $randomButton = document.querySelector('.button')
-    $randomButton.addEventListener('click', function (){
-        console.log('#####: Click')
-
-        changeHP(player1)
-        changeHP(player2)
-    })
